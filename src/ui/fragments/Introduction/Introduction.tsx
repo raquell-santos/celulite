f04b4e3e0560a0df06ac8ad3c6b6413f4c0ce61f
Daniel Quirino <danielpires6@hotmail.com>
2023-01-25 17:28:20 -0300

    wip

--- src/ui/fragments/Introduction/Introduction.tsx
+++ src/ui/fragments/Introduction/Introduction.tsx
@@ -10,17 +10,19 @@ import OLD from '../../assets/idosa.png'
 
 export default function Introduction() {
   return (
-
-
       <div className="flex flex-col text-center gap-x-2.5 gap-y-2.5">
           <Title>Acabe com suas celulites</Title>
-          <SubTitle>De acordo com o seu corpo</SubTitle>
+          <SubTitle>Você é único, e sua solução também precisa ser</SubTitle>
 
-          <div className="flex flex-col gap-x-3.5 gap-y-3.5">
-            <Card photo={TEEN} text={'18 - 29'}/>
-            <Card photo={ADULT} text={'30 - 39'}/>
-            <Card photo={MATURE} text={'40 - 49'}/>
-            <Card photo={OLD} text={'50+'}/>
+          <div className="flex flex-col gap-x-3.5 gap-y-6">
+            <div  className="flex gap-x-3.5 ">
+              <Card photo={TEEN} text={'18 - 29'}/>
+              <Card photo={ADULT} text={'30 - 39'}/>
+            </div>
+            <div  className="flex gap-x-3.5 ">
+              <Card photo={MATURE} text={'40 - 49'}/>
+              <Card photo={OLD} text={'50+'}/>
+            </div>
           </div>
       </div>
   )
