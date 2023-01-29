861bd7ac364175f5b7ab3ddcb20fce0775cf44f2
Daniel Quirino <danielpires6@hotmail.com>
2023-01-29 13:21:17 -0300

    wip

--- src/ui/page/Quiz/Quiz.tsx
+++ src/ui/page/Quiz/Quiz.tsx
@@ -22,25 +22,29 @@ export default function Quiz() {
   };
 
   return (
-    <div className="pt-8 pb-8 bg-slate-800 h-screen flex flex-col ">
-      <Stepper activeStep={activeStep} steps={6} handleNext={handleNext} handleBack={handleBack}/>
-      <Container>
-        {activeStep === 0 && <Inicial/>}
-        {activeStep === 1 && <Body />}
-        {activeStep === 2 && <Foods />}
-      </Container>
-      <div className="absolute w-full w-screen bottom-0">
-        <Button
-          variant="contained"
-          color='secondary'
-          fullWidth
-          onClick={handleNext}
-        >  
-          Próximo  
-        </Button>
-
-      </div>
-
-    </div>
+    <div className="bg-slate-800 ">
+        <div className="pt-8 pb-8 h-screen flex flex-col ">
+          <Stepper activeStep={activeStep} steps={6} handleNext={handleNext} handleBack={handleBack}/>
+          <Container>
+            {activeStep === 0 && <Inicial/>}
+            {activeStep === 1 && <Body />}
+            {activeStep === 2 && <Foods />}
+          </Container>
+        </div>
+        <div className="h-12"></div>
+          <div className="absolute fixed w-full w-screen -bottom-7 flex justify-center ">
+          <div className="w-full max-w-xs">
+            <Button
+              variant="contained"
+              color='secondary'
+              onClick={handleNext}
+              size='large'
+              fullWidth
+            >  
+              <span > Próximo </span>  
+            </Button>
+          </div>
+        </div>
+        </div>
   )
 }
