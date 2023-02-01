1a660e2feff0f9ac36f8df2f81db80e1b2e10c98
Daniel Quirino <danielpires6@hotmail.com>
2023-02-01 15:59:16 -0300

    wip

--- src/ui/components/Card/Primary/Primary.tsx
+++ src/ui/components/Card/Primary/Primary.tsx
@@ -1,13 +1,13 @@
 import { CheckCircle } from '@material-ui/icons'
 import { IPrimary} from './interface'
 
-export default function Primary({photo, text, isSelected = false, handleClick, id, height='28'}: IPrimary) {
+export default function Primary({photo, text, isSelected = false, handleClick, id, height}: IPrimary) {
   return (
     <div className="flex flex-col w-full">
       <div 
         onClick={() => handleClick(id)}
         className={`
-          group rounded-t border-material-pink border-2 h-${height} w-full shadow-md shadow-slate-200 flex justify-center 
+          group rounded-t border-material-pink border-2 ${height ? `h-${height}` : 'h-28' } w-full shadow-md shadow-slate-200 flex justify-center 
           hover:cursor-pointer hover:bg-slate-900 relative
           ${isSelected ? 'bg-slate-900': ''}
         `}
@@ -19,8 +19,8 @@ export default function Primary({photo, text, isSelected = false, handleClick, i
               alt="test" 
               className={`
                 h-full 
-                group-hover:scale-110 group-hover:-translate-y-2
-                ${isSelected ? 'scale-110 -translate-y-2': ''}
+                group-hover:scale-125 group-hover:-translate-y-3
+                ${isSelected ? 'scale-125 -translate-y-3': ''}
               `}/>
           </div>
       </div>
