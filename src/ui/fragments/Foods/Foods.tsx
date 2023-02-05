ccdbc0c7b52ff127e8454d0501b634376267ccc7
Daniel Quirino <danielpires6@hotmail.com>
2023-02-05 09:07:16 -0300

    wip

--- src/ui/fragments/Foods/Foods.tsx
+++ src/ui/fragments/Foods/Foods.tsx
@@ -1,34 +1,15 @@
 
 import SubTitle from "../../components/SubTitle";
-import Title from "../../components/Title";
 import Card from '../../components/Card/Secondary';
-
-import BANANA from '../../assets/foods/banana.png'
-import LEITE from '../../assets/foods/leite.png'
-import ALCOOL from '../../assets/foods/alcool.png'
-import SODA from '../../assets/foods/refrigerante.png'
-import CHOCOLATE from '../../assets/foods/chocolate.webp'
 import Question from "../../components/Question";
-import { useState } from "react";
 
 export default function Foods({foods, handleStep}) {
-  const [selecteds, setSelecteds] = useState<Array<number>>([])
-
-
-  const FOODS_OPTIONS = [
-    { photo:ALCOOL, text:'Bebidas alcoólicas', id: 1 },
-    { photo:BANANA, text:'Banana', id: 2 },
-    { photo:LEITE, text:'Leite', id: 3 },
-    { photo:CHOCOLATE, text:'Chocolate', id: 4 },
-    { photo:SODA, text:'Refrigerante', id: 5 },
-  ]
-
   const handleClick = (id: number): void => {
-    setSelecteds([...selecteds, id])
+    const foodIndex = foods.findIndex(food => food.id === id)
+    const food = foods.find(food => food.id === id);
+    foods[foodIndex] = {...food, isChecked: !food.isChecked}
 
-    const selectedsFoods = FOODS_OPTIONS.filter((food => selecteds.includes(food.id))).filter(i => i);
-
-    handleStep({ foods: selectedsFoods })
+    handleStep({ foods })
   }
 
   return (
@@ -40,41 +21,41 @@ export default function Foods({foods, handleStep}) {
 
           <div className="flex flex-col gap-x-3.5 gap-y-6 mb-8">
               <Card 
-                photo={FOODS_OPTIONS[0].photo} 
-                text={FOODS_OPTIONS[0].text}
-                id={FOODS_OPTIONS[0].id}
+                photo={foods[0].photo} 
+                text={foods[0].text}
+                id={foods[0].id}
                 handleClick={handleClick}
-                selected={selecteds.includes(FOODS_OPTIONS[0].id)}
+                selected={foods[0].isChecked}
               />
               <Card
-                photo={FOODS_OPTIONS[1].photo} 
-                text={FOODS_OPTIONS[1].text}
-                id={FOODS_OPTIONS[1].id}
+                photo={foods[1].photo} 
+                text={foods[1].text}
+                id={foods[1].id}
                 handleClick={handleClick}
-                selected={selecteds.includes(FOODS_OPTIONS[1].id)}
+                selected={foods[1].isChecked}
               />
               <Card 
-                photo={FOODS_OPTIONS[2].photo} 
-                text={FOODS_OPTIONS[2].text}
-                id={FOODS_OPTIONS[2].id}
+                photo={foods[2].photo} 
+                text={foods[2].text}
+                id={foods[2].id}
                 handleClick={handleClick}
-                selected={selecteds.includes(FOODS_OPTIONS[2].id)}
+                selected={foods[2].isChecked}
 
               />
               <Card 
-                photo={FOODS_OPTIONS[3].photo} 
-                text={FOODS_OPTIONS[3].text}
-                id={FOODS_OPTIONS[3].id}
+                photo={foods[3].photo} 
+                text={foods[3].text}
+                id={foods[3].id}
                 handleClick={handleClick}
-                selected={selecteds.includes(FOODS_OPTIONS[3].id)}
+                selected={foods[3].isChecked}
 
               />
               <Card 
-                photo={FOODS_OPTIONS[4].photo} 
-                text={FOODS_OPTIONS[4].text}
-                id={FOODS_OPTIONS[4].id}
+                photo={foods[4].photo} 
+                text={foods[4].text}
+                id={foods[4].id}
                 handleClick={handleClick}
-                selected={selecteds.includes(FOODS_OPTIONS[4].id)}
+                selected={foods[4].isChecked}
 
               />
           </div>
--- src/ui/page/Quiz/Quiz.tsx
+++ src/ui/page/Quiz/Quiz.tsx
@@ -7,10 +7,18 @@ import Weight from '../../fragments/Weight';
 import Drinks from '../../fragments/Drinks';
 import Result from '../../fragments/Result';
 
+import BANANA from '../../assets/foods/banana.png'
+import LEITE from '../../assets/foods/leite.png'
+import ALCOOL from '../../assets/foods/alcool.png'
+import SODA from '../../assets/foods/refrigerante.png'
+import CHOCOLATE from '../../assets/foods/chocolate.webp'
+
 import Stepper from '../../fragments/Stepper';
 import Body from '../../fragments/Body';
 import {  Button } from "@material-ui/core";
 import Loader from "../../fragments/Loader";
+
+import './Quiz.css';
 
 
 export default function QuizPage() {
@@ -23,11 +31,17 @@ export default function QuizPage() {
       id: 0,
     },
     drink: {
-      value: '',
+      text: '',
       photo: '',
       id: 0,
     },
-    foods: [],
+    foods: [
+      { photo:ALCOOL, text:'Bebidas alcoólicas', id: 1, isChecked: false },
+      { photo:BANANA, text:'Banana', id: 2, isChecked: false },
+      { photo:LEITE, text:'Leite', id: 3, isChecked: false },
+      { photo:CHOCOLATE, text:'Chocolate', id: 4, isChecked: false },
+      { photo:SODA, text:'Refrigerante', id: 5, isChecked: false },
+    ],
     height: 1.6,
     weight: 70,
     loaded: false,
@@ -72,11 +86,26 @@ export default function QuizPage() {
       disabled = !hasWeight;
     }
 
-    if(activeStep>2) disabled = false
+    if(activeStep === 3 ) {
+      const hasBodies = data.bodies.filter(body => body.isChecked).filter(i=>i).length > 0
+      disabled = !hasBodies;
+    }
 
-    // if(activeStep === 6) {
-    //   disabled = !data.loaded;
-    // }
+    if(activeStep === 4 ) {
+      const hasFoods = data.foods.filter(food => food.isChecked).filter(i=>i).length > 0
+      disabled = !hasFoods;
+    }
+
+    if(activeStep === 5 ) {
+      console.log(data.drink)
+
+      const hasDrink = data.drink.id
+      disabled = !hasDrink;
+    }
+
+    if(activeStep === 6) {
+      disabled = !data.loaded;
+    }
 
     return disabled;
   }
@@ -94,23 +123,24 @@ export default function QuizPage() {
 
   return (
     <div className="bg-slate-800 ">
-        <div className="pt-8 pb-8 h-screen flex flex-col ">
+        <div className="pt-8 pb-16 h-screen flex flex-col ">
           <Stepper activeStep={activeStep} steps={6} handleNext={handleNext} handleBack={handleBack}/>
           <Container>
             {activeStep === 0 && <Inicial age={data.age} handleStep={handleClick}/>}
             {activeStep === 1 && <Height height={data.height} handleStep={handleClick} />}
             {activeStep === 2 && <Weight weight={data.weight} handleStep={handleClick} />}
             {activeStep === 3 && <Body bodies={data.bodies} handleStep={handleClick}/>}
-            {activeStep === 4 && <Foods foods={[]} handleStep={handleClick} />}
+            {activeStep === 4 && <Foods foods={data.foods} handleStep={handleClick} />}
             {activeStep === 5 && <Drinks drink={data.drink} handleStep={handleClick}/>}
             {activeStep === 6 && <Loader handleStep={handleClick} />}
             {activeStep === 7 && <Result data={data}/>}
  
           </Container>
         </div>
-          <div className="absolute fixed w-full w-screen bottom-0 flex justify-center ">
-          <div className="w-full max-w-xs">
+          <div className="absolute fixed w-full w-screen bottom-0 flex justify-center h-16">
+          <div className="w-full max-w-xs submit-button flex items-center">
             <Button
+            className="submit-button"
               variant="contained"
               color='secondary'
               onClick={handleNext}
